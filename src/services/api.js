60068dbe865c76ac4b42cc01fc4e32bfbd3b6b81
luizh3nrique <luizh3nrique00@gmail.com>
2023-07-18 22:08:56 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const key = '92d3c00770d5e5ed839fb991cb6efa0695202f1e';
+export const key = 'a4e943fadcddf08b3af10ab93d0036a3fed951d9';
 
 const api = axios.create({
     baseURL: "https://api-ssl.bitly.com/v4", headers: {
@@ -9,3 +9,6 @@ const api = axios.create({
 })
 
 export default api
+
+
+//codigo de api acabou em 18/07 poderar usar 18/08 = 92d3c00770d5e5ed839fb991cb6efa0695202f1e
